a8ef2bcef37a56daf086ae26184aab090bee96a7
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-12-24 12:34:28 -0500

    feat: Add notes on python's glob module

---
+++ python/globbing.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Two different ways to loop over files with glob:
+    glob.iglob('*'): Returns an iterator
+    glob.glob('*'): Returns a list of matching filenames
+    
+"""
+import glob
+
+if __name__ == '__main__':
+    
+
+    for item in glob.iglob('*'):
+        match item:
+            case item if item.endswith('.py'):
+                print(f"Python File found: {item}")
+            case item if item.endswith('.txt'):
+                print(f"Text File found: {item}")
+            case item if item.endswith('.md'):
+                print(f"Markdown File found: {item}")
+            case _:
+                print(f"Other file found: {item}")
+
+##########################################################
+
+    files = glob.glob('*')
+    for item in files:
+        match item:
+            case item if item.endswith('.py'):
+                print(f"Python File found: {item}")
+            case item if item.endswith('.txt'):
+                print(f"Text File found: {item}")
+            case item if item.endswith('.md'):
+                print(f"Markdown File found: {item}")
+            case _:
+                print(f"Other file found: {item}")
+
