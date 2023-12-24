156ce30bda55e7989c88213cd704bbaafb32d4a7
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-12-24 12:39:22 -0500

    feat: Add notes on switch case (match and case keywords) in python

---
+++ python/switch_case.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Python 3.10+ has the `match` keyword which can 
+be used like `switch` in other languages.
+
+case _: 
+    This is the default case (case * in other languages)
+
+A `break` statement isn't needed in any case.
+"""
+
+import string
+
+if __name__=='__main__':
+    letters = string.ascii_lowercase
+    print(f"Letters:\n{letters}")
+    for l in letters:
+        match l:
+            case 'a':
+                print('a')
+            case 'b':
+                print('b')
+            case 'c':
+                print('c')
+            case _:
+                print('other')
+                break
+                # Break out of the loop
+
+    
