d384d4d5ded53239a50aa00852ff04e91099ff03
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2024-03-05 12:22:55 -0500

    feat: Add more snippets for dark mode pdf viewing

--- web/javascript/pdf_dark_mode.js
+++ web/javascript/pdf_dark_mode.js
@@ -1,7 +1,7 @@
-/* 
- *  This function will invert the colors of the current page.  
- *  Useful for viewing PDF files in dark mode. 
- *  Only tested in Firefox.  
+/*
+ *  This function will invert the colors of the current page.
+ *  Useful for viewing PDF files in dark mode.
+ *  Only tested in Firefox.
  * */
 
 javascript: (function () {
@@ -9,3 +9,16 @@ javascript: (function () {
   el.style.filter = "grayscale(1) invert(1) sepia(1) contrast(75%)";
 })();
 
+/* if grayscale(1) and sepia(1) are too harsh
+ * - try 'filter:  invert(1) contrast(50%)'
+ *   inverts the blacks and whites and adds a contrast of 50% so you get a nice grey backdrop against a white-ish text. */
+javascript: (function () {
+  var el = typeof viewer !== "undefined" ? viewer : document.body;
+  el.style.filter = "invert(1) contrast(50%)";
+})();
+
+/*  Clear dark mode by removing the filter.  */
+javascript: (function () {
+  var el = typeof viewer !== "undefined" ? viewer : document.body;
+  el.style.filter = "";
+})();
