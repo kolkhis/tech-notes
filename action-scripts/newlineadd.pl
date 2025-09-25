3074c20f4ffe0ea30bbc8b699df2a7c2c8a96e88
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-09-25 12:10:20 -0400

    docs: Add minimal description header

--- action-scripts/newlineadd.pl
+++ action-scripts/newlineadd.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/env perl
-# Add a newline before list items (for mkdocs)
+# Add a newline before the first ordered/unordered list item in a list
+# and markdown tables to make MkDocs happy.
+# Can be used as a rough translator for GitHub flavored Markdown
+# to MkDocs flavored Markdown.  
 
 my @markdown_files = <./**/*.md>;
 my $newline_pattern = qr/^(?:\s*[*-]|\s*\d{1,}\.|[|])/;
-# my $prev_pattern = qr/^(?!\s*\n|\s*[-*]|[|])/;
 
 for my $file (@markdown_files) {
     local $^I = '';
