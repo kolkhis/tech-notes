88de5b1f7c1567a31759c312aa13f4a86d8b6696
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-09-26 18:26:09 -0400

    refactor: Use more idiomatic perl conditional syntax
    Change to inline postfix conditionals for single-condition blocks.
    
    Switch to core Data::Dumper and use empty import list to prevent
    polluted namespace.
    
    Use ternary operator to define @markdown_files.
    
    Use local typeglob alias 'dump' for Data::Dumper::Dumper.

--- scripts/newlineadd.pl
+++ scripts/newlineadd.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/env perl
+# Author: Connor W. (https://github.com/kolkhis)
 # Add a newline before the first ordered/unordered list item in a list
 # and markdown tables to make MkDocs happy.
 # Can be used as a rough translator for GitHub flavored Markdown
@@ -6,23 +7,21 @@
 
 use strict;
 use warnings;
-use Data::Dump;
+use Data::Dumper ();
+local *dump = \&Data::Dumper::Dumper;
 
 my @markdown_files;
 
-if (!@ARGV) {
-    print "No arguments provided. Searching docs/...\n";
-    @markdown_files = <./docs/**/*.md>;
-} else {
-    print "File arguments found. Using filenames provided.\n";
-    @markdown_files = grep { -f $_ && /\.md$/ } @ARGV;
-}
+print "File arguments found. Using filenames provided.\n" if @ARGV;
+print "No arguments provided. Searching docs/...\n" unless @ARGV;
 
-print "Markdown files being converted:\n" . Data::Dump::dump(\@markdown_files) . "\n";
+@markdown_files = @ARGV 
+    ? grep { -f $_ && /\.md$/ } @ARGV 
+    : <./docs/**/*.md>;
 
-if (!@markdown_files) {
-    die "[ERROR]: No files found for perl to modify!\n";
-}
+print "Markdown files being converted:\n" . main::dump(\@markdown_files) . "\n";
+
+die "[ERROR]: No files found for perl to modify!\n" unless @markdown_files;
 
 my $newline_pattern = qr/^(?:\s*[*-]|\s*\d{1,}\.|[|])/;
 my $prev_pattern = qr/$newline_pattern|^\n/;
@@ -33,12 +32,11 @@ for my $file (@markdown_files) {
 
     my $prev = '';
     while (<>) {
-        if (m/$newline_pattern/ && $prev !~ m/($prev_pattern)/) {
-            print "\n";
-        }
+        print "\n" if (m/$newline_pattern/ && $prev !~ m/($prev_pattern)/);
         print;
         $prev = $_;
     }
+
 }
 
 print "Conversion is complete.\n";
